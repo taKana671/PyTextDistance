ce0fa782844b7ab6b2a2674a4e4c874f9c3c5d15
KanaeOhta <kanae5321@gmail.com>
2020-06-27 18:59:22 +0900

    Modified to use memoryview for speed-up

--- cython_code/Levenshtein/cylevenshtein.pyx
+++ cython_code/Levenshtein/cylevenshtein.pyx
@@ -1,4 +1,9 @@
+from cython import boundscheck, wraparound
+import numpy as np
 
+
+@boundscheck(False)
+@wraparound(False)
 def levenshtein(str s1, str s2):
     
     cdef int s1_len = len(s1)
@@ -6,7 +11,10 @@ def levenshtein(str s1, str s2):
     cdef int s1_range = s1_len + 1
     cdef int s2_range = s2_len + 1 
     cdef int i, j, d1, d2, d3
-
+    cdef int[:, ::1] arr
+    
+    arr = np.zeros((s1_range, s2_range), dtype=np.int32)
+    
     if s1 == s2:
         return 0
 
@@ -14,17 +22,23 @@ def levenshtein(str s1, str s2):
         return s2_len
     if s2_len == 0:
         return s1_len
-    
-    arr = [[j for j in range(s2_range)] if i == 0 \
-        else [i if j == 0 else 0 for j in range(s2_len+1)] for i in range(s1_range)]
-    
 
+    for i in range(s2_range):
+        arr[0, i] = i
+    
+    for i in range(s1_range):
+        arr[i, 0] = i
+
+    
     for i in range(1, s1_range):
         for j in range(1, s2_range):
-            d1 = arr[i-1][j] + 1
-            d2 = arr[i][j-1] + 1
-            d3 = arr[i-1][j-1] + (0 if s1[i-1]==s2[j-1] else 1)
-            arr[i][j] = min(d1, d2, d3)
+            d1 = arr[i-1, j] + 1
+            d2 = arr[i, j-1] + 1
+            d3 = arr[i-1, j-1] + (0 if s1[i-1]==s2[j-1] else 1)
+            arr[i, j] = min(d1, d2, d3)
 
 
-    return arr[s1_len][s2_len]
+    return arr[s1_len, s2_len]
+
+
+
