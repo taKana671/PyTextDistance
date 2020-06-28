f0fed67d8a08f86e5daea2f7498a9ba7369f2b89
KanaeOhta <kanae5321@gmail.com>
2020-06-28 23:10:51 +0900

    Modified to use cython array, instead of ndarray

--- cython_code/Levenshtein/cylevenshtein.pyx
+++ cython_code/Levenshtein/cylevenshtein.pyx
@@ -1,19 +1,22 @@
+from cython.view cimport array
 from cython import boundscheck, wraparound
-import numpy as np
+
+
+def levenshtein(str s1, str s2)
+    return distance(tuple(s1), tuple(s2))
 
 
 @boundscheck(False)
 @wraparound(False)
-def levenshtein(str s1, str s2):
-    
+cdef int distance(tuple s1, tuple s2):
+
     cdef int s1_len = len(s1)
     cdef int s2_len = len(s2)
     cdef int s1_range = s1_len + 1
     cdef int s2_range = s2_len + 1 
-    cdef int i, j, d1, d2, d3
+    cdef int i, j
     cdef int[:, ::1] arr
-    
-    arr = np.zeros((s1_range, s2_range), dtype=np.int32)
+    cdef str word
     
     if s1 == s2:
         return 0
@@ -23,21 +26,22 @@ def levenshtein(str s1, str s2):
     if s2_len == 0:
         return s1_len
 
+    arr = array(shape=(s1_range, s2_range), itemsize=sizeof(int), format='i')
     for i in range(s2_range):
         arr[0, i] = i
     
     for i in range(s1_range):
         arr[i, 0] = i
-
-    
+ 
     for i in range(1, s1_range):
+        word = s1[i-1]
         for j in range(1, s2_range):
-            d1 = arr[i-1, j] + 1
-            d2 = arr[i, j-1] + 1
-            d3 = arr[i-1, j-1] + (0 if s1[i-1]==s2[j-1] else 1)
-            arr[i, j] = min(d1, d2, d3)
-
-
+            arr[i, j] = min(
+                arr[i-1, j] + 1,
+                arr[i, j-1] + 1,
+                arr[i-1, j-1] + (0 if word==s2[j-1] else 1)
+            )
+          
     return arr[s1_len, s2_len]
 
 
