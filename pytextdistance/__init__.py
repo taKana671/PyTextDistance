9073a88dfe88649ae87da193d3c3bd72bdbae417
KanaeOhta <kanae5321@gmail.com>
2020-07-14 20:47:45 +0900

    Added cyhamming module

--- pytextdistance/__init__.py
+++ pytextdistance/__init__.py
@@ -1,17 +1,18 @@
 from collections import OrderedDict
 
-from cytextdistance.cylevenshtein import levenshtein
-from cytextdistance.cyjarowinkler import jaro, jaro_winkler
-from cytextdistance.tabulator import Tabulator
+from pytextdistance.cylevenshtein import levenshtein
+from pytextdistance.cyjarowinkler import jaro, jaro_winkler
+from pytextdistance.cyhamming import hamming
+from pytextdistance.tabulator import Tabulator
 
 
 def compare_distance(str1, str2):
     if not isinstance(str1, str) or not isinstance(str2, str):
         raise TypeError('Arguments must be str.')
     items = OrderedDict()
-    items['string1'], items['string2']= str1, str2  
     for key, func in zip(('Levenshtein', 'Jaro-Winkler'), (levenshtein, jaro_winkler)):
         items[key] = func(str1, str2)
+    items['hamming'] = hamming(str1, str2) if len(str1) == len(str2) else '*'
     tabulator = Tabulator(items)
     print(tabulator.tabulate())
 
