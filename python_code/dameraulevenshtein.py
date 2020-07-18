e49bc80325115efe752c2004953a822840e59f93
taKana671 <kanae5321@gmail.com>
2020-07-19 00:42:01 +0900

    Modified so as to correctly compute Damerau-Levenshtein distance between "abcdef" and "abcfad"

--- python_code/dameraulevenshtein.py
+++ python_code/dameraulevenshtein.py
@@ -20,7 +20,7 @@ def damerau_levenshtein(s1, s2):
     for i in range(2, len_s1 + 2):
         temp = 1
         for j in range(2, len_s2 + 2):
-            k = charas.get(s2[j-2], 1)
+            k = charas.get(s2[j-2], 1) # last row with matching character
             l = temp
             cost = 0 if s1[i-2] == s2[j-2] else 1
             if not cost:
@@ -29,22 +29,28 @@ def damerau_levenshtein(s1, s2):
                 arr[i][j-1] + 1, # insertion
                 arr[i-1][j] + 1, # deletion                        
                 arr[i-1][j-1] + cost, # substitution
-                arr[k-1][l-1] + (i-k-1) + 1 + (j-l-1) # transposition
+                # arr[k-1][l-1] + (i-k-1) + 1 + (j-l-1) # transposition
+                arr[k-1][l-1] + max((i-k-1),(j-l-1)) + 1 # transposition
                 )
         charas[s1[i-2]] = i
 
-    for ar in arr:
-        print(ar)
+    # for ar in arr:
+    #     print(ar)
         
     return arr[-1][-1]
 
     
 if __name__ == '__main__':
-    # print(damerau_levenshtein('kitten', 'sitting'))
-    # print(damerau_levenshtein('abcdef', 'abcfad'))
-    # print(damerau_levenshtein('ca', 'abc'))
-    # print(damerau_levenshtein('a cat', 'a abct'))
-    # print(damerau_levenshtein('a cat', 'an act'))
-    # print(damerau_levenshtein('ifhs', 'fish'))
+    print(damerau_levenshtein('kitten', 'sitting'))
+    print(damerau_levenshtein('abcdef', 'abcfad'))
+    print(damerau_levenshtein('ca', 'abc'))
+    print(damerau_levenshtein('a cat', 'a abct'))
+    print(damerau_levenshtein('a cat', 'an act'))
+    print(damerau_levenshtein('ifhs', 'fish'))
     print(damerau_levenshtein('BADC', 'ABCD'))
     print(damerau_levenshtein('ZX', 'XYZ'))
+    print(damerau_levenshtein('BADC', ''))
+    print(damerau_levenshtein('', 'ABCD'))
+    print(damerau_levenshtein('ABCD', 'ABCD'))
+    print(damerau_levenshtein('', ''))
+    print(damerau_levenshtein('太田佳苗', '田太苗佳'))
