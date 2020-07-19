e45cade3b190ffe3356486ac4bf01961a0415ba0
taKana671 <kanae5321@gmail.com>
2020-07-19 18:01:07 +0900

    Added cydameraulevenshtein module

--- pytextdistance/__init__.py
+++ pytextdistance/__init__.py
@@ -1,6 +1,7 @@
 from pytextdistance.cylevenshtein import levenshtein
 from pytextdistance.cyjarowinkler import jaro, jaro_winkler
 from pytextdistance.cyhamming import hamming
+from pytextdistance.cydameraulevenshtein import damerau_levenshtein
 from pytextdistance.tabulator import Tabulator
 from pytextdistance.distance import *
 
@@ -8,8 +9,10 @@ from pytextdistance.distance import *
 def compare_distance(str1, str2):
     if not isinstance(str1, str) or not isinstance(str2, str):
         raise TypeError('Arguments must be str.')
-    header = ['Levenshtein', 'Jaro-Winkler', 'Hamming']
-    results = [func(str1, str2) for func in (levenshtein, jaro_winkler)]
+    header = ['Levenshtein', 'Damerau_Levenshtein', 
+        'Jaro', 'Jaro-Winkler', 'Hamming']
+    results = [func(str1, str2) for func in (
+        levenshtein, damerau_levenshtein, jaro, jaro_winkler)]
     results.append(hamming(str1, str2) if len(str1) == len(str2) else '*')
     tabulator = Tabulator([header, results])
     print(tabulator.tabulate())
