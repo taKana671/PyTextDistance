d9ff179891b23e5b58cb88051a1d35c01a44e3a1
taKana671 <kanae5321@gmail.com>
2020-07-20 21:47:06 +0900

    Added test for Damerau_Levenshtein distance

--- tests/test_distance.py
+++ tests/test_distance.py
@@ -1,6 +1,11 @@
 import unittest
-from cytextdistance import levenshtein, jaro, jaro_winkler
-
+from pytextdistance import (
+    levenshtein, 
+    jaro, 
+    jaro_winkler, 
+    hamming, 
+    damerau_levenshtein
+)
 
 class TestLevenshtein(unittest.TestCase):
 
@@ -45,7 +50,7 @@ class TestJaro(unittest.TestCase):
 
 class TestJaroWinkler(unittest.TestCase):
 
-    def test_jaro(self):
+    def test_jaro_winkler(self):
         self.assertEqual(jaro_winkler('abc', 'bac'), 0.8888888888888888)
         self.assertEqual(jaro_winkler('dicksonx', 'dixon'), 0.8133333333333332)
         self.assertEqual(jaro_winkler('dixon', 'dicksonx'), 0.8133333333333332)
@@ -66,5 +71,42 @@ class TestJaroWinkler(unittest.TestCase):
         self.assertRaises(TypeError, jaro, 5, 1)
 
 
+class TestHamming(unittest.TestCase):
+
+    def test_hamming(self):
+        self.assertEqual(hamming('karolin', 'kathrin'), 3)
+        self.assertEqual(hamming('karolin', 'kerstin'), 3)
+        self.assertEqual(hamming('kathrin', 'kerstin'), 4)
+        self.assertEqual(hamming('1011101', '1001001'), 2)
+        self.assertEqual(hamming('2173896', '2233796'), 3)
+        self.assertEqual(hamming('Brian', 'Jesus'), 5)
+        self.assertEqual(hamming('Brian', 'Brian'), 0)
+
+
+    def test_invalid_args(self):
+        self.assertRaises(TypeError, hamming, 'abc', 1)
+        self.assertRaises(ValueError, hamming, 'abc', 'abcdf')
+
+
+class TestDamerauLevenshtein(unittest.TestCase):
+
+    def test_damerau_levenshtein(self):
+        self.assertEqual(damerau_levenshtein('abcdef', 'abcfad'), 2)
+        self.assertEqual(damerau_levenshtein('ca', 'abc'), 2)
+        self.assertEqual(damerau_levenshtein('a cat', 'a abct'), 2)
+        self.assertEqual(damerau_levenshtein('a cat', 'an act'), 2)
+        self.assertEqual(damerau_levenshtein('ifhs', 'fish'), 2)
+        self.assertEqual(damerau_levenshtein('BADC', 'ABCD'), 2)
+        self.assertEqual(damerau_levenshtein('ZX', 'XYZ'), 2)
+        self.assertEqual(damerau_levenshtein('BADC', ''), 4)
+        self.assertEqual(damerau_levenshtein('', 'ABCD'), 4)
+        self.assertEqual(damerau_levenshtein('ABCD', 'ABCD'), 0)
+        self.assertEqual(damerau_levenshtein('', ''), 0)
+
+
+    def test_invalid_args(self):
+        self.assertRaises(TypeError, hamming, 'abc', 1)
+
+ 
 if __name__ == '__main__':
     unittest.main()
