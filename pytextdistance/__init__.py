c591aa04870cbb4e8ae565378345e84562a92541
taKana671 <kanae5321@gmail.com>
2020-08-16 22:44:16 +0900

    Added normalized_levenshtein

--- pytextdistance/__init__.py
+++ pytextdistance/__init__.py
@@ -1,4 +1,4 @@
-from pytextdistance.cylevenshtein import levenshtein
+from pytextdistance.cylevenshtein import levenshtein, normalized_levenshtein
 from pytextdistance.cyjarowinkler import jaro, jaro_winkler
 from pytextdistance.cyhamming import hamming
 from pytextdistance.cydameraulevenshtein import damerau_levenshtein
@@ -10,10 +10,10 @@ from pytextdistance.outputfile import ExcelHandler, CsvHandler, TextFileHandler
 def compare_distances(str1, str2):
     if not isinstance(str1, str) or not isinstance(str2, str):
         raise TypeError('Arguments must be str.')
-    header = ['Levenshtein', 'Damerau_Levenshtein', 
+    header = ['Levenshtein', 'Damerau_Levenshtein', 'Normalized_Levenshtein'
         'Jaro', 'Jaro-Winkler', 'Hamming']
     results = [func(str1, str2) for func in (
-        levenshtein, damerau_levenshtein, jaro, jaro_winkler)]
+        levenshtein, damerau_levenshtein, normalized_levenshtein, jaro, jaro_winkler)]
     results.append(hamming(str1, str2) if len(str1) == len(str2) else '*')
     tabulator = Tabulator([header, results])
     print(tabulator.tabulate())
