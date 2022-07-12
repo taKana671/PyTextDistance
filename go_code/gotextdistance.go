0ce566740adf3fd4243e79e4bf48b0b81f974447
taKana671 <kanae5321@gmail.com>
2022-07-13 01:09:23 +0900

    Modified Levenshtein.

--- go_code/gotextdistance.go
+++ go_code/gotextdistance.go
@@ -1,7 +1,7 @@
 package main
 
 import "C"
-import "fmt"
+import "math"
 
 //export Hamming
 func Hamming(s1 string, s2 string) int {
@@ -30,22 +30,37 @@ func Levenshtein(s1 string, s2 string) int {
 	rs1Len := len(rs1)
 	rs2Len := len(rs2)
 
-	arr := make([][]uint, rs1Len+1)
+	arr := make([][]int, rs1Len+1)
 
 	for i := range arr {
-		sub := make([]uint, rs2Len+1)
+		sub := make([]int, rs2Len+1)
 		if i == 0 {
 			for j := range sub {
-				sub[j] = uint(j)
+				sub[j] = j
 			}
 		} else {
-			sub[0] = uint(i)
+			sub[0] = i
 		}
 		arr[i] = sub
 	}
 
-	fmt.Println(arr)
-	return rs2Len
+	for i := 1; i < rs1Len+1; i++ {
+		for j := 1; j < rs2Len+1; j++ {
+			d1 := arr[i-1][j] + 1
+			d2 := arr[i][j-1] + 1
+
+			n := 0
+			if rs1[i-1] != rs2[j-1] {
+				n = 1
+			}
+
+			d3 := arr[i-1][j-1] + n
+			arr[i][j] = math.Min(d1, d2, d3)
+
+		}
+	}
+
+	return arr[rs1Len][rs2Len]
 }
 
 func main() {
